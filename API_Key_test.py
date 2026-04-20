c38f169a786b57a5e26281f3549c6955406cb691
Teja swaroop <Tejaswaroopcse@gmail.com>
2026-04-21 01:01:12 +0530

    Update API key to a placeholder in test file
    Replaced hardcoded API key with a placeholder.

--- API_Key_test.py
+++ API_Key_test.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = "30627522cfe8fdf8166fd84f89a9b95b"
+API_KEY = "YOUR API KEY"
 
 url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}"
 response = requests.get(url, timeout=10)
